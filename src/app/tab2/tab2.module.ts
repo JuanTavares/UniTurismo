c2632396327f8cb47a8c4087557c46993b2fc592
Juan Tavares <juantavares2006@hotmail.com>
2019-08-22 23:48:23 -0300

    Maps

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    GoogleMaps,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,11 @@
+import { GoogleMaps, GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +15,46 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  map: GoogleMap;
+
+  constructor() { }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+  Environment.setEnv({
+    'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+    'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+  });
+
+  const mapOptions: GoogleMapOptions = {
+    camera: {
+       target: {
+         lat: 43.0741904,
+         lng: -89.3809802
+       },
+       zoom: 18,
+       tilt: 30
+     }
+  };
+
+  this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+  const marker: Marker = this.map.addMarkerSync({
+    title: 'Ionic',
+    icon: 'blue',
+    animation: 'DROP',
+    position: {
+      lat: 43.0741904,
+      lng: -89.3809802
+    }
+  });
+
+  marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+    alert('clicked');
+  });
+}
 
 }
