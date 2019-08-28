4b50d3f324a13355525b8163d4672cf2c106dbea
Juan Tavares <juantavares2006@hotmail.com>
2019-08-27 22:01:05 -0300

    Configurando google maps

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { GoogleMaps } from '@ionic-native/google-maps';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    GoogleMaps,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,60 +1,67 @@
-import { GoogleMaps, GoogleMap,
+import {
+  GoogleMaps,
+  GoogleMap,
   GoogleMapsEvent,
   GoogleMapOptions,
   CameraPosition,
   MarkerOptions,
   Marker,
   Environment
-} from '@ionic-native/google-maps';
-import { Component } from '@angular/core';
+} from '@ionic-native/google-maps/ngx';
+import { Component, ViewChild, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements AfterContentInit {
+
+  @ViewChild('Map', { static: false }) myMap;
 
   map: GoogleMap;
 
   constructor() { }
 
-  ionViewDidLoad() {
+  ngAfterContentInit(): void {
     this.loadMap();
   }
 
   loadMap() {
-  Environment.setEnv({
-    'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
-    'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
-  });
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '',
+      'API_KEY_FOR_BROWSER_DEBUG': ''
+    });
 
-  const mapOptions: GoogleMapOptions = {
-    camera: {
-       target: {
-         lat: 43.0741904,
-         lng: -89.3809802
-       },
-       zoom: 18,
-       tilt: 30
-     }
-  };
+    const mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
 
-  this.map = GoogleMaps.create('map_canvas', mapOptions);
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
 
-  const marker: Marker = this.map.addMarkerSync({
-    title: 'Ionic',
-    icon: 'blue',
-    animation: 'DROP',
-    position: {
-      lat: 43.0741904,
-      lng: -89.3809802
-    }
-  });
+    const marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
 
-  marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-    alert('clicked');
-  });
-}
+  logador(data: any) {
+    console.log(data);
+  }
 
 }
